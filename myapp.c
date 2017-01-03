0c1d804cf833e02a5b673ae801701f925e3b13b8
sasha00s <services@7cats.biz>
2017-01-04 00:56:05 +0400

    Add files via upload

---
+++ myapp.c
@@ -0,0 +1,53 @@
+
+/*********************************************************************************************
+ *                                                                                           *
+ *  File: myapp.c                                                                            *
+ *  Type: main program.                                                                      *
+ *  Distribution: source/object code.                                                        *
+ *  License: GNU General Public License ver3.                                                *
+ *  Dependency: ,                                                                            *
+ *  Desription: .                                                                            *
+ *                                                                                           *
+ *********************************************************************************************
+ *                                                                                           *
+ *     This is myapp.c file (main program).                                                  *
+ *     Copyleft, 2016-2017, <feedback@7cats.biz>, cats7.                                     *
+ *                                                                                           *
+ *     This program is free software; you can redistribute it and/or modify it under the     *
+ *  terms of the GNU General Public License as published by the Free Software Foundation;    *
+ *  either version 3 of the License, or (at your option) any later version.                  *
+ *                                                                                           *
+ *     This program is distributed in the hope that it will be useful, but WITHOUT ANY       *
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A          *
+ *  PARTICULAR PURPOSE.  See the GNU General Public License for more details.                *
+ *                                                                                           *
+ *********************************************************************************************/
+
+/*********************************************************************************************
+ *                                *** Header Files ***                                       *
+ *********************************************************************************************/
+
+#include "myapp.h"
+
+/*********************************************************************************************
+ *                              *** Global Variables ***                                     *
+ *********************************************************************************************/
+
+
+/*********************************************************************************************
+ *                             *** Function Prototypes ***                                   *
+ *********************************************************************************************/
+
+
+/*********************************************************************************************
+ *                                *** main Function ***                                      *
+ *********************************************************************************************/
+
+int main (int argc, char *argv[]){
+
+  return(EXIT_SUCCESS); }
+
+/*********************************************************************************************
+ *                                *** End of File ***                                        *
+ *********************************************************************************************/
+
